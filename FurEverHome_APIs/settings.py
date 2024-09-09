a535c4ff0161881b1cc712dd73f373a429875af2
Rocky Chowdhury <rocky20809@gmail.com>
2024-09-09 21:16:56 +0600

    project setup completed

--- FurEverHome_APIs/settings.py
+++ FurEverHome_APIs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure--ame12h5g)oogtm7cwm!51b3322uy(yl=m*qr@qntyz)2)@x2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Accounts',
 ]
 
 MIDDLEWARE = [
